354afef9b5895bbe7de4d1ddf23291284398d31c
Rogelio Macedo <rmace001@ucr.edu>
2019-11-07 03:20:20 -0800

    Render Heatmap using Highcharts API closes #37 * Render a heatmap of rougly 27000 data points * As of now the javascprit is hardcoded into the html * Research is needed to convert this chart into an Angular component * datapoints are in object form which will work well with backend

--- dummyCelldata.cc
+++ dummyCelldata.cc
@@ -5,12 +5,32 @@
 #include <ctime>
 
 
+std::string getColor(float val) {
+    if (val < 0.5){
+        return "#3060cf";
+    }
+    else if(val >= 0.5 && val <= 0.6){
+        return "#fffbbc";
+    }
+    else {
+        return "#c4463a";
+    }
+}
 
 int main(){
     srand (static_cast <unsigned> (time(0)));
-    for (int i = 0; i < 8000; i++){
-        
-        std::cout << "Event Outcome Block Year " + std::to_string((static_cast <float> (rand()) / static_cast <float> (RAND_MAX))) + "\n"; 
+    float value; 
+    for (int i = 0; i < 558; i++){
+        for (int j = 0; j < 47; j++){
+            value = (static_cast <float> (rand()) / static_cast <float> (RAND_MAX));
+            std::cout << "{\n\tx: " << j << ",\n\ty: " << i << ",\n\tvalue: " + std::to_string(value) + ",\n\tname: \'E102\'\n}";
+            if (i == 557 && j == 46){
+                std::cout << "\n";
+            }
+            else{
+                std::cout << ",\n";
+            }
+        }
     }
     return 0;
 }
