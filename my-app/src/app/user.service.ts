e1e7b221cc160aa3ccf86a7412dd4a149047581a
MarkBis415 <thebisness@DESKTOP-C6KBVGQ.localdomain>
2019-11-24 17:57:56 -0800

    search()

--- my-app/src/app/user.service.ts
+++ my-app/src/app/user.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/toPromise';
+import {Http, Headers, Response} from '@angular/http';
 
 class SearchItem {
     constructor(
         public userid: string,
-        public userFisrt: string,
+        public userFirst: string,
         public userLast: string,
         public userName: string,
         public userPass: string 
@@ -21,13 +23,30 @@ export class UserService {
     serverURLi = "http://localhost:4000" // this is the server the backend is running from 
     constructor(private http: HttpClient) {
         this.results = [];
+        this.loading = false;
     }
 
-    // search(term: string){
-    //     let promise = new Promise((resolve, reject) => {
-    //         let ourURL = `${this.serverURLi}?term=`
-    //     })
-    // }
+    search(term: string){
+        let promise = new Promise((resolve, reject) => {
+            let ourURL = `${this.serverURLi}?term=${term}`;
+            this.http.get(ourURL).toPromise().then(res => {
+                this.results = res.results.map(item =>{
+                    return new SearchItem(
+                        item.id,
+                        item.fname,
+                        item.lname,
+                        item.user,
+                        item.pass
+                    );
+                });
+                resolve();
+            },
+            msg => {
+                reject(msg);
+            });
+        });
+        return promise;
+    }
 
     postCelltoRun(f:String, l:String, u:String, p:String)
     {
