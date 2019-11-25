cfe921f7a66ca7a47f8a074b6a19b42ced2e8abb
shiyao feng <32398815+Windbless@users.noreply.github.com>
2019-11-25 00:23:26 -0800

    Keep local changes before master merging into my branch * drop down menu of outcome topics * pick one topic display all event score

--- my-app/src/app/display-chart/display-chart.component.ts
+++ my-app/src/app/display-chart/display-chart.component.ts
@@ -11,6 +11,7 @@ import * as CanvasJS from '../../assets/canvasjs.min.js';
 import { MatSort, MatSortable, MatPaginator, MatTableDataSource } from '@angular/material';
 import { chart } from 'highcharts/highcharts.src';
 import {BloopComponent} from '../bloop/bloop.component';
+import { element } from 'protractor';
 @Component({
   selector: 'app-display-chart',
   templateUrl: './display-chart.component.html',
@@ -19,6 +20,7 @@ import {BloopComponent} from '../bloop/bloop.component';
 export class DisplayChartComponent implements OnInit {
   @ViewChild(BloopComponent, {static:true}) bloop_c;
   runchoiceName: string = ""
+  outcomeName = ""
   outcomed
   DataSource1
   OutcomeList
@@ -26,6 +28,7 @@ export class DisplayChartComponent implements OnInit {
   ScoreList
   alllist
   chart
+  check = false
   constructor(private runService: RunService, private router: Router) { }
   r: Run[] // class has element r of type array of Runs
   getData(){ // returns list of objects i.e. the datalist for my chart
@@ -39,18 +42,79 @@ export class DisplayChartComponent implements OnInit {
                             y:i,
                             value: this.ScoreList[i][1][j],
                             name: 'The Cell',
-                  
                             custom: {event: eventNames, outcome: this.OutcomeList[j]}
                         });
         }
     }
     return datalist;
   }
-  selectChangeHandler (runchoice) {
-    //update the ui
-    console.log("pick up")
-  }
+   doAdelay(){
+    setTimeout(function(){return true;},300000);
+};
+  onOptionsSelected(value:string){
+    console.log("the selected value is " + value);
+    var local_outcomelist = this.OutcomeList
+    console.log(local_outcomelist)
+    var index =0;
+    index = local_outcomelist.indexOf(value)
+    console.log( "Here is ", index, " The string is ", local_outcomelist[index])
+    this.outcomeName = value
+    console.log(this.ScoreList)
+    var list_display_score = this.ScoreList
+    console.log(this.ScoreList[0][0])
+    var dps = []; // dataPoints
+    let chart3 = new CanvasJS.Chart("chartContainer3", {
+      animationEnabled: true,
+      exportEnabled: true,
+      title: {
+        text: this.DataSource1.data[0].Event
+      },
+      axisX :{ labelFontColor: "transparent", labelFormatter: function(e) {
+        return "";
+      }},
+      axisY: { prefix: "Run_score ", includeZero: false },
+      data: [{
+        type: "column",
+        legendMarkerType: "triangle",
+        legendMarkerColor: "green",
+        color: "rgba(255,12,32,.3)",
+        showInLegend: false,
+        legendText: "Score of Run",
+        dataPoints : dps
 
+      }]
+    });
+    if(!dps){
+      this.doAdelay();
+    };  
+    var length_list = Object.keys(this.ScoreList).length
+    console.log(length_list)
+    console.log("Here is y ",this.ScoreList[1][0])
+    console.log("Here is label ",this.ScoreList[1][1][0])
+      for (var j = 0; j < length_list; j++) {	
+                  dps.push({
+        y: this.ScoreList[j][1][index],
+        label: this.ScoreList[j][0]
+      });
+          
+    }; 
+    this.check = true 
+    if(dps){
+      this.doAdelay();
+    };  
+    console.log(" hi true 2 ", this.check)
+    if(this.check === true){
+      console.log(" hi ")
+      chart3.render();
+      this.chart == false
+    };
+    
+      // this.check = 1
+     
+    // for(var row = 0; row < this.ScoreList.length ;row++){
+    //   console.log("name", this.ScoreList[row][0], " the score is ")
+    // }
+}
   ngOnInit() {
   //  console.log("Start here ", this.runService.runName)
    this.runchoiceName = this.runService.runName
@@ -69,7 +133,7 @@ export class DisplayChartComponent implements OnInit {
       }
       this.outcomed = list_o
       this.OutcomeList = input_listOutcomes
-      console.log(this.OutcomeList)
+      // console.log(this.OutcomeList)
        this.runService.showEventNames(this.runchoiceName).subscribe((val: Run[]) => //send http request and results are subscribed into val
     {
      
@@ -110,6 +174,12 @@ export class DisplayChartComponent implements OnInit {
         final_row_Event_cell_list[row] = [input_list_score[row].Event,insidelist]
       }
       this.ScoreList = final_row_Event_cell_list
+
+      // for(var i = 0; i <  input_list_score.length; i++){
+      //   console.log(this.ScoreList[i][0], " score is ", this.ScoreList[i][0][0])
+      // }
+
+
       var dps = []; // dataPoints
       for (var j = 0; j < this.OutcomeList.length; j++) {	
                   dps.push({
@@ -134,7 +204,7 @@ export class DisplayChartComponent implements OnInit {
                     dataPoints: dps
                   }]
                 });
-        chart1.render();          
+               
   
       // this.alllist = this.ScoreList
       let chart2 = new CanvasJS.Chart("chartContainer2", {
@@ -143,6 +213,9 @@ export class DisplayChartComponent implements OnInit {
                   title: {
                     text: this.DataSource1.data[0].Event
                   },
+                  axisX :{ labelFontColor: "transparent", labelFormatter: function(e) {
+                    return "";
+                  }},
                   axisY: { prefix: "Run_score ", includeZero: false },
                   data: [{
                     type: "column",
@@ -157,6 +230,7 @@ export class DisplayChartComponent implements OnInit {
                 });
             
         chart2.render();
+        chart1.render();  
                 
     })
     })
